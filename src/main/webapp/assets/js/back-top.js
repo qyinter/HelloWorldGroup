435d1747dbaf33845545e9efb5456c965eba33dd
tsusura173 <tsusura173@gmail.com>
2021-10-24 07:23:41 +0900

    update animation

--- src/main/webapp/assets/js/back-top.js
+++ src/main/webapp/assets/js/back-top.js
@@ -1,20 +1,20 @@
 let mybutton = document.getElementById("back-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+// window.onscroll = function () {
+//   scrollFunction();
+// };
 
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 500 ||
-    document.documentElement.scrollTop > 500
-  ) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (
+//     document.body.scrollTop > 500 ||
+//     document.documentElement.scrollTop > 500
+//   ) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
